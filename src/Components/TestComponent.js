c769c84fe1423d339009f708d396f1ff5caf1eca
Bassel saleh <bacell94@gmail.com>
2022-12-30 14:54:01 +0200

    install chakraUI, framer-motion, create TestComponent

---
+++ src/Components/TestComponent.js
@@ -0,0 +1,29 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
+function TestComponent() {
+    return(
+        <>
+            <Tabs>
+                <TabList>
+                    <Tab>One</Tab>
+                    <Tab>Two</Tab>
+                    <Tab>Three</Tab>
+                </TabList>
+
+                <TabPanels>
+                    <TabPanel>
+                    <p>one!</p>
+                    </TabPanel>
+                    <TabPanel>
+                    <p>two!</p>
+                    </TabPanel>
+                    <TabPanel>
+                    <p>three!</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </>
+    )
+}
+
+export default TestComponent;
